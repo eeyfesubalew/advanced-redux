88e5bff685f238d57de991780d13b926d56208c7
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-13 09:04:43 -0800

    adding add to cart feature

--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -8,22 +8,34 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItme = state.items.find((item) => item.id === newItem.id);
-
-      if (!existingItme) {
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
-          quantity: newItem.quantity,
+          quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title,
         });
       } else {
-        existingItme.quantity++;
-        existingItme.totalPrice = existingItme.totalPrice + newItem.price;
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
 
+export const cartActions = cartSlice.actions;
 export default cartSlice;
